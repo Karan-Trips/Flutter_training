702a3aa47eb80e40b8b396b9496682577af8b0b1
KARAN SHANKAR TRIPATHI <karanboss652@gmail.com>
2024-02-02 11:02:31 +0530

    Digits of number

---
+++ digit.dart
@@ -0,0 +1,26 @@
+// Find digits of entered number using switch case
+
+// import 'dart:io';
+
+// void main() {
+//   stdout.write("Enter the number:");
+//   String no = stdin.readLineSync()!;
+//   finddigit(no);
+// }
+
+// void finddigit(String digit) {
+//   List<String> str = digit.split("");
+//   print(str);
+//   for (int i = str.length - 1; i >= 0; i--) {
+//     switch (int.parse(str[i])) {
+//       case 1:
+//         print("one");
+//         break;
+//       case 0:
+//         print("zero");
+//         break;
+//       default:
+//         throw Exception("no is not zero nor 1");
+//     }
+//   }
+// }
