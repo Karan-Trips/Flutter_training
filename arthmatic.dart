82d73df4f0362337bdce989a0652c84f099c5ac5
KARAN SHANKAR TRIPATHI <karanboss652@gmail.com>
2024-02-02 10:53:12 +0530

    arthmetic

---
+++ arthmatic.dart
@@ -0,0 +1,37 @@
+// =-----------------------------------TASK 2-----------------
+
+// void main(List<String> args) {
+//   int a = int.parse(args[0]);
+//   int b = int.parse(args[1]);
+// // Relational
+//   print("if a equal b = ${a == b}");
+//   print("if a greater b = ${a > b}");
+//   print("if b is greateer = ${a < b}");
+//   print("if a not equal b = ${a != b}");
+//   // Increment & decrements
+//   a++;
+//   print("Increments = ${a}");
+//   b--;
+//   print("decrements = ${b}");
+// // logical
+//   if (a == 0 && b == 0) {
+//     print("Both are zero");
+//   } else if (a == 0 || b == 0) {
+//     print(" a is zero here =$a");
+//   } else {
+//     print("b is zero = $b");
+//   }
+//   // Arithmateic
+//   print("Sum = ${a + b}");
+//   print("sub = ${a - b}");
+//   print("mul = ${a * b}");
+//   print("div = ${a ~/ b}");
+
+//   // Casceding notation
+
+//   List<int> lst = [];
+//   lst
+//     ..add(a)
+//     ..add(b);
+//   print(lst);
+// }
