5066d387ce756c29db29926cebe9d79ac44b2eb9
KARAN SHANKAR TRIPATHI <karanboss652@gmail.com>
2024-02-02 10:58:27 +0530

    Swap program

---
+++ swap.dart
@@ -0,0 +1,12 @@
+
+// =-----------------------------------TASK 3-----------------
+
+// void main(List<String> arg) {
+//   int a = int.parse(arg[0]);
+//   int b = int.parse(arg[1]);
+//   print("before swap = $a $b");
+//   int temp = a;
+//   a = b;
+//   b = temp;
+//   print("Swap = $a $b");
+// }
