7f30f5b6e60df9c357dc0d0ebc9ea36366cfe23c
KARAN SHANKAR TRIPATHI <karanboss652@gmail.com>
2024-02-02 10:57:42 +0530

    max & min

---
+++ contian.dart
@@ -0,0 +1,20 @@
+// void main(List<String> arg) {
+//   /// length of string
+//   int len = arg[0].length;
+
+//   String str1 = arg[1];
+
+//   print("Length of =$len");
+//   // combine
+//   String com = arg[0] + " " + arg[1];
+
+//   print("combined = $com");
+//   //  Lower case
+//   print("Lower case :${str1.toLowerCase()}");
+//   // Upper case
+
+//   print("Upper  case :${str1.toUpperCase()}");
+
+//   print("COntian =${arg[0].contains(str1)}");
+
+// }
---
+++ maxmin.dart
@@ -0,0 +1,34 @@
+// void main(List<String> str) {
+//   List<int> lst = [];
+//   for (String i in str) {
+//     lst.add(int.parse(i));
+//   }
+//   int max = lst[0];
+//   int min = 0;
+//   for (int i = 0; i < lst.length; i++) {
+//     if (max > lst[i]) {
+//       max = lst[i];
+
+//     } else if (max < lst[i]) {
+//       min = lst[i];
+//     }
+//   }
+//   print("Min is = $max");
+//   print(min);
+// }
+// -------------------------------------------
+// void main(List<String> str) {
+//   List<int> lst = [];
+//   for (String i in str) {
+//     lst.add(int.parse(i));
+//   }
+//   int max = lst[0];
+//   for (int i in lst) {
+
+//     if (i > max) {
+//       max = i;
+//     }
+//   }
+
+//   print("max is = $max");
+// }
