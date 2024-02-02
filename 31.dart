aca27e1af86b6b59190096bf79126eba32acccb6
KARAN SHANKAR TRIPATHI <karanboss652@gmail.com>
2024-02-02 09:48:58 +0530

    R&D git

---
+++ 31.dart
@@ -0,0 +1,12 @@
+
+// Git and Github
+
+///git init
+///git add .
+///git git commit -m "message to pass"
+///git status 
+///git checkout -b branch_name
+///git push origin branch_name
+///git log -3   --------detail of 3 latest update made
+///git pull origin branch_name
+///
